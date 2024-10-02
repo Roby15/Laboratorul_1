bc9cc84c8ecf4ad4cc7924bc3e24ee589fcdf4dd
Roby15 <trifasrobert5@gmail.com>
2024-10-02 18:49:28 +0300

    Laborator 1 PJ

---
+++ src/Ex4/Problema4.java
@@ -0,0 +1,33 @@
+package Ex4;
+
+import java.util.Random;
+
+public class Problema4 {
+    public static void main(String[] args) {
+
+        Random rand = new Random();
+        int a = rand.nextInt(31);
+        int b = rand.nextInt(31);
+        System.out.println("a="+a);
+        System.out.println("b="+b);
+        int ok=0,i=0;
+        if(a>b)
+            i=a;
+        else if(a<b)
+            i=b;
+        while (ok==0)
+        {
+            if(a%i==0 && b%i==0)
+            {
+                System.out.println("Cmmdc:"+i);
+                ok=1;
+                break;
+            }
+
+            i-=1;
+        }
+        if(i==1) {
+            System.out.println("Unul dintre numere e prim");
+        }
+    }
+}
