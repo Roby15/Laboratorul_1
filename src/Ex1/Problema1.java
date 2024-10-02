89599a98e2021eb25b365190a8d134d937564c6c
Roby15 <trifasrobert5@gmail.com>
2024-10-02 18:16:09 +0300

    Laborator 1 PJ

--- src/Ex1/Problema1.java
+++ src/Ex1/Problema1.java
@@ -1,8 +1,19 @@
 package Ex1;
 
+import java.util.Scanner;
+
 public class Problema1 {
     public static void main(String[] args)
     {
+        Scanner scanner=new Scanner(System.in);
+        System.out.print("Lungime:");
+        int lun=scanner.nextInt();
+        System.out.print("Latime:");
+        int lati=scanner.nextInt();
+        int A=lun*lati;
+        int P=2*lun+2*lati;
 
+        System.out.println("Perimetru:"+P);
+        System.out.println("Aria:"+A);
     }
 }
